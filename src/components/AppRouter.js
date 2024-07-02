2cad7acb148c149b4ab9af4600a02b3a36ba6985
Legros Jordan <legros.jordan@live.fr>
2024-07-02 23:36:33 +0200

    Ajout des pages produits

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -3,12 +3,15 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import AboutPage from "../pages/APropos";
 import NotFound from "../pages/NotFound";
+import CardPage from "../pages/CardPage.js";
+
 
 const AppRouter = () => {
     return (
         <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/about" element={<AboutPage />} />
+            <Route path="/card/:id" element={<CardPage />} />
             <Route path="*" element={<NotFound />} />
         </Routes>
     );
--- src/components/CardsContainer.js
+++ src/components/CardsContainer.js
@@ -1,13 +1,24 @@
+import { useNavigate } from 'react-router-dom';
 import React from "react";
 import Card from "./Card";
 import "../styles/CardsContainer.scss";
 import data from "../assets/backend/donnees.json"; 
 
 const CardsContainer = () => {
+  const navigate = useNavigate();
+
+  const handleCardClick = (id) => {
+    console.log("click");
+    navigate(`/card/${id}`);
+  };
+
+
   return (
     <div className="CardsContainer">
       {data.map((location) => (
-        <Card key={location.id} title={location.title} />
+        <button key={location.id} onClick={() => handleCardClick(location.id)}>
+          <Card key={location.id} title={location.title}  />
+          </button>
       ))}
     </div>
   );
